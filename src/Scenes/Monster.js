eb31d52a4068a499fe4ed4116ad5ed9796c354a6
xf3s <77856194+xf3s@users.noreply.github.com>
2025-04-23 12:52:16 -0700

    wip

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -6,7 +6,44 @@ class Monster extends Phaser.Scene {
         //Create constants for the monster location
         this.bodyX = 300;
         this.bodyY = 350;
+
+        // arms
+        this.armRightX = this.bodyX + 90;
+        this.armRightY = this.bodyY + 30;
+
+        this.armLeftX = this.bodyX - 90;
+        this.armLeftY = this.bodyY + 30;
+
+        // legs
+        this.legRightX = this.bodyX + 60;
+        this.legRightY = this.bodyY + 100;
+
+        this.legLeftX = this.bodyX - 60;
+        this.legLeftY = this.bodyY + 100;
+
+        // eyes
+        this.eyeRightX = this.bodyX;
+        this.eyeRightY = this.bodyY;
         
+        this.eyeLeftX = this.bodyX;
+        this.eyeLeftY = this.bodyY;
+
+        // sensory
+        this.noseX = this.bodyX;
+        this.noseY = this.bodyY;
+
+        this.earRightX = this.bodyX;
+        this.earRightY = this.bodyY;
+
+        this.earLeftX = this.bodyX;
+        this.earLeftY = this.bodyY;
+
+        // mouths
+        this.mouthSmileX = this.bodyX;
+        this.mouthSmileY = this.bodyY;
+
+        this.mouthFangsX = this.bodyX;
+        this.mouthFangsY = this.bodyY;   
     }
 
     // Use preload to load art and sound assets before the scene starts running.
@@ -25,15 +62,31 @@ class Monster extends Phaser.Scene {
     create() {
         let my = this.my;   // create an alias to this.my for readability
 
+
         // Create the main body sprite
         //
         // this.add.sprite(x,y, "{atlas key name}", "{name of sprite within atlas}")
         //
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
-
         
+
+        my.sprite.armRight = this.add.sprite(this.armRightX, this.armRightY, "monsterParts", "arm_darkE.png");
+        my.sprite.armRight.angle = -30;
+
+        my.sprite.armLeft = this.add.sprite(this.armLeftX, this.armLeftY, "monsterParts", "arm_yellowE.png");
+        my.sprite.armLeft.flipX = true;
+        my.sprite.armLeft.angle = 30;
+        
+        my.sprite.legRight = this.add.sprite(this.legRightX, this.legRightY, "monsterParts", "leg_greenE.png")
+    
+        my.sprite.legLeft = this.add.sprite(this.legLeftX, this.legLeftY, "monsterParts", "leg_blueC.png")
+        my.sprite.legLeft.flipX = true;
+        
+
+
+        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_redF.png");
+
     }
 
     update() {
